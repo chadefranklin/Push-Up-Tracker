42ecc77d3db9003e105587695f631c4f919404c5
Chad Franklin <chadfranklin@fb.com>
2020-07-23 11:18:53 -0400

    Added PushupViewController

---
+++ Push-Up-Tracker/View Controllers/PushupViewController.h
@@ -0,0 +1,18 @@
+//
+//  PushupViewController.h
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/23/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PushupViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Push-Up-Tracker/View Controllers/PushupViewController.m
@@ -0,0 +1,32 @@
+//
+//  PushupViewController.m
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/23/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import "PushupViewController.h"
+
+@interface PushupViewController ()
+
+@end
+
+@implementation PushupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
