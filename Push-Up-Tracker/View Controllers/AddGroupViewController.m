14ac875ee9444cdb68e5967172ca5cd3c70de290
Chad Franklin <chadfranklin@fb.com>
2020-07-23 13:30:28 -0400

    Added AddGroupViewController

---
+++ Push-Up-Tracker/View Controllers/AddGroupViewController.m
@@ -0,0 +1,51 @@
+//
+//  AddGroupViewController.m
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/23/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import "AddGroupViewController.h"
+
+@interface AddGroupViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *joinJoinCodeField;
+@property (weak, nonatomic) IBOutlet UITextField *createGroupNameField;
+@property (weak, nonatomic) IBOutlet UITextField *createJoinCodeField;
+
+@end
+
+@implementation AddGroupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onJoinPressed:(id)sender {
+    
+}
+
+- (IBAction)onCreatePressed:(id)sender {
+    
+}
+
+- (IBAction)onJoinFieldEdited:(id)sender {
+    
+}
+
+- (IBAction)onCreateFieldEdited:(id)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
