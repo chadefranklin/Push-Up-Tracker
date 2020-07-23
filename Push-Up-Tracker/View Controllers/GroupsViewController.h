eb1cacdae67f1da6b4675a356f1a29d022d3608c
Chad Franklin <chadfranklin@fb.com>
2020-07-23 13:30:46 -0400

    Added GroupsViewController

---
+++ Push-Up-Tracker/View Controllers/GroupsViewController.h
@@ -0,0 +1,17 @@
+//
+//  GroupsViewController.h
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/23/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GroupsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Push-Up-Tracker/View Controllers/GroupsViewController.m
@@ -0,0 +1,32 @@
+//
+//  GroupsViewController.m
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/23/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import "GroupsViewController.h"
+
+@interface GroupsViewController ()
+
+@end
+
+@implementation GroupsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
