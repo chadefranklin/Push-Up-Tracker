d8a599ee4e26933f457d300d5603a7bdf7efc425
Chad Franklin <chadfranklin@fb.com>
2020-07-24 14:27:16 -0400

    Added GroupCell View Model

---
+++ Push-Up-Tracker/Views/GroupCell.h
@@ -0,0 +1,17 @@
+//
+//  GroupCell.h
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/24/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GroupCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Push-Up-Tracker/Views/GroupCell.m
@@ -0,0 +1,24 @@
+//
+//  GroupCell.m
+//  Push-Up-Tracker
+//
+//  Created by chadfranklin on 7/24/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import "GroupCell.h"
+
+@implementation GroupCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
