cfef23758020bb736a8508f7920f34bb350a3710
Chad Franklin <chadfranklin@fb.com>
2020-07-24 15:12:02 -0400

    Updated GroupCell View Model

--- Push-Up-Tracker/Views/GroupCell.h
+++ Push-Up-Tracker/Views/GroupCell.h
@@ -7,11 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Group.h"
+@import Parse;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GroupCell : UITableViewCell
 
+@property (weak, nonatomic) IBOutlet PFImageView *groupImageView;
+@property (weak, nonatomic) IBOutlet UILabel *groupNameLabel;
+
+@property (strong, nonatomic) Group *group;
+
+- (void)setGroup:(Group *)group;
 @end
 
 NS_ASSUME_NONNULL_END
--- Push-Up-Tracker/Views/GroupCell.m
+++ Push-Up-Tracker/Views/GroupCell.m
@@ -13,12 +13,23 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    
+    self.groupImageView.layer.masksToBounds = YES;
+    self.groupImageView.layer.cornerRadius = self.groupImageView.bounds.size.width / 2;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)setGroup:(Group *)group {
+    _group = group;
+    //self.groupImageView.file = group[@"image"];
+    //[self.groupImageView loadInBackground];
+    
+    self.groupNameLabel.text = group.name;
 }
 
 @end
